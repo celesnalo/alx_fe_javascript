6b23f2f591267b3c2570ecaf91813b9a1b8c3d39
Mthobisi Snalo Cele <celersnalor0119@gmail.com>
2024-07-27 18:00:25 +0200

    modify js file

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -10,7 +10,7 @@ function showRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomIndex];
   const quoteDisplay = document.getElementById('quoteDisplay');
-  quoteDisplay.innerText = `${randomQuote.text} - ${randomQuote.category}`;
+  quoteDisplay.innerHTML = `${randomQuote.text} - ${randomQuote.category}`;
 }
 
 // Function to add a new quote
